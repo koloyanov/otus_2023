cc7a1d51aeafc6f1160fcd2b1c1e0fe3c39ac153
koloyanov <kolivev@gmail.com>
2023-06-30 22:13:44 +0300

    L1 first commit

---
+++ L01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -0,0 +1,9 @@
+package ru.otus;
+
+import com.google.common.math.DoubleMath;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        System.out.println(DoubleMath.isPowerOfTwo(32.0));
+    }
+}
