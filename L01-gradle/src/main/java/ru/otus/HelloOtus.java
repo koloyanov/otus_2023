e2e54caa78806f6a6a518ffecbcab7743d9f3b6a
koloyanov <kolivev@gmail.com>
2023-07-19 20:24:20 +0300

    add generics module

--- L01-gradle/src/main/java/ru/otus/HelloOtus.java
+++ L01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -2,6 +2,7 @@ package ru.otus;
 
 import com.google.common.math.DoubleMath;
 
+@SuppressWarnings("java:S106")
 public class HelloOtus {
     public static void main(String[] args) {
         System.out.println(DoubleMath.isPowerOfTwo(32.0));
---
+++ L04-generics/src/main/java/ru/otus/Main.java
@@ -0,0 +1,8 @@
+package ru.otus;
+
+@SuppressWarnings("java:S106")
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
